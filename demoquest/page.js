e58aac56bef1c69505ec8c6fbfe2fe1275c22ac9
Peter Rogers <peter.rogers@gmail.com>
2018-01-07 12:40:53 -0500

    Added demoquest build

---
+++ demoquest/page.js
@@ -0,0 +1,36 @@
+function onload()
+{
+    var div = document.getElementById("canvas_area");
+    var width = window.innerWidth-5;
+    var height = window.innerHeight-5;
+    div.style.width = width + "px";
+    div.style.height = height + "px";
+
+    demoquest.configure(div);
+
+    window.addEventListener("resize", resize);
+
+    setTimeout(resize, 100);
+}
+
+function resize()
+{
+    var div = document.getElementById("canvas_area");
+    var width = window.innerWidth-5;
+    var height = window.innerHeight-5;
+
+    var credits = document.getElementById("credits");
+    var creditsRect = credits.getBoundingClientRect();
+    height -= creditsRect.height;
+
+    div.style.width = width + "px";
+    div.style.height = height + "px";
+    demoquest.resize();
+}
+
+function closeCredits()
+{
+    var div = document.getElementById("credits");
+    div.style.display = "none";
+    resize();
+}
